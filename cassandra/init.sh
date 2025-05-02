41286115f3137fed5ae566be9260a92ac16a251f
VolkovskayaUliana <uliana.volkovskaya@smekalka.com>
2025-05-02 16:53:04 +0300

    feat(migrations): bitrix example fill data

--- cassandra/init.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-until printf "" 2>>/dev/null >>/dev/tcp/cassandra/9042; do
-    sleep 5;
-    echo "Waiting for cassandra...";
-done
-
-echo "Creating keyspace and table..."
-cqlsh cassandra -u cassandra -p cassandra -f "/docker-entrypoint-initdb.d/init.cql"
--- core-processor/src/main/kotlin/Main.kt
+++
@@ -1,5 +0,0 @@
-package ru.modula.core
-
-fun main() {
-    println("Hello World!")
-}
